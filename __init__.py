756e42ca2c81aaf766b8025cca0ecde8470338c3
ftorres1795 <felix.torres@kavak.com>
2023-05-28 19:18:44 -0300

    route protection and todo list index

--- __init__.py
+++ __init__.py
@@ -20,7 +20,11 @@ def create_app():
     from . import auth
 
     app.register_blueprint(auth.bp)
-    
+
+    from . import todo
+
+    app.register_blueprint(todo.bp)
+
     @app.route('/hola')
     def hola():
         return 'hola'
